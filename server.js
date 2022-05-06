ee79d5f256b5c358a2ddd24846c9d9a371b438c5
dev <dev@dev>
2022-05-06 07:18:26 -0700

    get work items

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import * as azdev from "azure-devops-node-api"
 import 'dotenv/config' // This line can be removed if run with: node -r dotenv/config server.js
-// import * as WorkItemTrackingInterfaces from 'azure-devops-node-api/interfaces/WorkItemTrackingInterfaces.js'
-// import * as wit from 'azure-devops-node-api/interfaces/WorkItemTracking.js'
 
 const token = process.env.AZURE_PERSONAL_ACCESS_TOKEN
 const orgUrl = process.env.API_URL
@@ -21,30 +19,21 @@ const coreApiObject = await connection.getCoreApi()
 const project = await coreApiObject.getProject(process.env.API_PROJECT)
 
 console.log(`team ${project.defaultTeam.name}`)
-const teamContext = {project: project.name,
-    projectId: project.id,
-    team: project.defaultTeam.name,
-    teamId: project.defaultTeam.id}
 
-// console.log('Overview of recent activity');
-// console.log('Work data in progress', await witApi.getAccountMyWorkData(WorkItemTrackingInterfaces.QueryOption.Doing))
-// console.log('Recent Activity:', await witApi.getRecentActivityData())
-// console.log('Recent Mentions:', await witApi.getRecentMentions())
+const query = await witApi.getQuery(project.id, "Shared Queries/Team/EB/PI EB User Stories")
+const items = await witApi.queryById(query.id)
 
-console.log('Get work item info');
-const queries = await witApi.getQueries(project.id)
-console.log('There are', queries.length, 'queries')
-if (queries.length > 0) {
-    console.log('Sample query:', queries[0])
-}
+const columNames = items.columns.map(_ => _.name)
+const fields = items.columns.map(_ => _.referenceName)
+const allIds = items.workItems.map(_ => _.id)
 
-console.log('Get work item info');
-const workItemTypes = await witApi.getWorkItemTypes(project.id);
-console.log('Work item types:', workItemTypes.map((item) => item.name));
-if (workItemTypes.length > 0) {
-    const type = workItemTypes[0];
-    console.log('Info for type' + type.name);
-    console.log(type.name, 'has', (await witApi.getWorkItemTypeColors([project.name])).length, 'colors');
+let allWorkItems = []
+let idsRemaining = allIds
+const chunk = 200
+while (idsRemaining.length > 0) {
+    const ids = idsRemaining.filter((_, i) => i < chunk)
+    allWorkItems = allWorkItems.concat(await witApi.getWorkItemsBatch({fields, ids}))
+    idsRemaining = idsRemaining.filter((_, i) => i >= chunk)
 }
 
 console.log("done")
