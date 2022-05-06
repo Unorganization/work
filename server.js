7c33538e03aadf2f87dc98132eb1d53ff6c318ab
dev <dev@dev.com>
2022-05-05 20:15:16 -0700

    .

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-import * as azdev from "azure-devops-node-api";
+import * as azdev from "azure-devops-node-api"
 import 'dotenv/config' // This line can be removed if run with: node -r dotenv/config server.js
+import * as ba from "azure-devops-node-api/BuildApi"
+import * as bi from "azure-devops-node-api/interfaces/BuildInterfaces"
 
-let orgUrl = "https://dev.azure.com/AVEVA-VSTS/";
+let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN
+let orgUrl = process.env.API_URL
 
-let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
-if (!token) {
-    console.log(`Missing envvar: AZURE_PERSONAL_ACCESS_TOKEN`)
+if (!token || !orgUrl) {
+    console.log(`Required environment variables not set`)
     process.exit(1)
 }
 
-let authHandler = azdev.getPersonalAccessTokenHandler(token); 
-let connection = new azdev.WebApi(orgUrl, authHandler);    
+let authHandler = azdev.getPersonalAccessTokenHandler(token) 
+let connection = new azdev.WebApi(orgUrl, authHandler)    
+
+let build = await connection.getBuildApi()
 
 console.log("done")
