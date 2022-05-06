dcdf3f269a94d159621e9237d4689974aff7282b
dev <dev@dev.com>
2022-05-05 20:00:17 -0700

    add .env support

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log('Hello.  Arguments: ', process.argv.slice(2));
+import * as azdev from "azure-devops-node-api";
+import 'dotenv/config' // This line can be removed if run with: node -r dotenv/config server.js
+
+let orgUrl = "https://dev.azure.com/AVEVA-VSTS/";
+
+let token = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
+if (!token) {
+    console.log(`Missing envvar: AZURE_PERSONAL_ACCESS_TOKEN`)
+    process.exit(1)
+}
+
+let authHandler = azdev.getPersonalAccessTokenHandler(token); 
+let connection = new azdev.WebApi(orgUrl, authHandler);    
+
+console.log("done")
