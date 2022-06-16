f22b5c52b272f8ab5ba24bd3b73fea780b7fe921
dev <dev@dev>
2022-06-16 03:45:35 -0700

    sqlite

--- test/sql/sql_test.js
+++ test/sql/sql_test.js
@@ -1,8 +1,49 @@
 const assert = require("assert");
 
 describe("sql_test", function () {
-  it("does x when Y", function () {
-    assert.equal(1, 19, "aaa");
-    assert.equal([1, 2, 3].indexOf(4), -1);
+  it("basic", function () {
+    const sqlite3 = require("sqlite3").verbose();
+    const db = new sqlite3.Database(":memory:");
+
+    db.serialize(() => {
+      db.run("CREATE TABLE lorem (info TEXT)");
+
+      const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+      for (let i = 0; i < 3; i++) {
+        stmt.run("Ipsum " + i);
+      }
+      stmt.finalize();
+      console.log("aaa");
+      db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
+      });
+    });
+
+    db.close();
+  });
+
+  it("join", function () {
+    const sqlite3 = require("sqlite3").verbose();
+    const db = new sqlite3.Database(":memory:");
+
+    db.serialize(() => {
+      db.run("CREATE TABLE person (name TEXT)");
+      db.run("CREATE TABLE pet (name TEXT, person_name TEXT)");
+
+      db.run("INSERT INTO person VALUES ('Bob')");
+      db.run("INSERT INTO person VALUES ('Alice')");
+
+      db.run("INSERT INTO pet (name, person_name) VALUES ('Fidoz', 'Bob')");
+
+      db.each("SELECT rowid AS id, name FROM person", (err, row) => {
+        console.log(row.id + ": " + row.name);
+      });
+      db.each("SELECT rowid AS id, name FROM pet", (err, row) => {
+        console.log(row.id + ": " + row.name + ", " + row.person_name);
+        console.dir(row);
+      });
+    });
+
+    db.close();
   });
 });
