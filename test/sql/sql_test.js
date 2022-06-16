433f6688d4b6d63e4a05792bbaa22c2c0b5045af
dev <dev@dev>
2022-06-16 02:19:10 -0700

    .

---
+++ test/sql/sql_test.js
@@ -0,0 +1,8 @@
+const assert = require("assert");
+
+describe("sql_test", function () {
+  it("does x when Y", function () {
+    assert.equal(1, 19, "aaa");
+    assert.equal([1, 2, 3].indexOf(4), -1);
+  });
+});
